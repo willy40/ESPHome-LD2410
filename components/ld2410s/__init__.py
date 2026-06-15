4fb513a4ea096a033a595722ebba9271b84fe4e6
arek.gabrys <arek.gabrys@gmail.com>
2026-06-15 18:57:25 +0200

    draft

--- components/ld2410s/__init__.py
+++ components/ld2410s/__init__.py
@@ -44,10 +44,7 @@ GATE_ENERGY_SCHEMA = sensor.sensor_schema(
     icon="mdi:alpha-e-box",
 )
 
-GATE_ENERGY_WRITE_SCHEMA = number.number_schema(
-    LD2410SNumber,
-    step=1,
-)
+GATE_ENERGY_WRITE_SCHEMA = number.number_schema(LD2410SNumber)
 
 CONFIG_SCHEMA = (
     cv.Schema(
@@ -125,7 +122,7 @@ async def to_code(config):
         cg.add(var.set_gate_energy_sensor(i, sens))
 
     for i, gate_cfg in enumerate(config.get(CONF_GATE_ENERGY_WRITE, [])):
-        num = await number.new_number(gate_cfg)
+        num = await number.new_number(gate_cfg, min_value=0, max_value=65535)
         await cg.register_component(num, gate_cfg)
         cg.add(num.set_parent(var))
         cg.add(num.set_role(3))
